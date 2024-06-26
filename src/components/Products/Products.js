5a1c7e3bba928acb4c0e7038b4e236fd3af71737
Aarya01Patil <147030857+Aarya01Patil@users.noreply.github.com>
2024-06-26 22:30:22 +0530

    Optimized the Cart function using dispatcher

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -2,11 +2,11 @@ import ProductCard from "../ProductCard/ProductCard";
 import Effect from "../Effect/Effect";
 import React from "react";
 import { useState, useEffect } from "react";
-import useWindowSize from "../../hooks/useWindowSize";
+import useWindow from "../../hooks//UseWindow/useWindow";
 function Products() {
   let [products, setProducts] = useState([]);
   console.log("products")
-  console.log(useWindowSize());
+  console.log(useWindow());
   useEffect(() => {
     fetch("https://602fc537a1e9d20017af105e.mockapi.io/api/v1/products").then(
       (response) => {
--- src/components/ReduxAddToCart/ReduxAddToCart.jsx
+++ src/components/ReduxAddToCart/ReduxAddToCart.jsx
@@ -1,31 +1,32 @@
 import { useSelector, useDispatch } from "react-redux";
-function ReduxAddToCart({ product }) {
-    let dispatch = useDispatch();
-    function increase() {
-        dispatch({ type: "ADD_TO_CART", payload: product });
-    }
-    function decrease() {
-     dispatch({ type: "REMOVE_FROM_CART", payload: product });
-    }
+import { addToCart,removeFromCart } from "../../store";
 
-    let quantity = useSelector((state) => {
+function ReduxAddToCart({product}){
+    let dispatch = useDispatch();
+    function increase(){
+        dispatch(addToCart(product));
+    }
+    function decrease(){
+        dispatch(removeFromCart(product));
+    }
+    let quantity = useSelector((state)=>{
         return state.items[product.id]?.quantity || 0;
     })
-
-    if (quantity === 0) {
-        return (
+    if(quantity === 0){
+        return(
             <div>
                 <button onClick={increase}>AddToCart</button>
             </div>
-     )  
-    } else {
-        return ( 
-            <div>
-                <button onClick={decrease}>-</button>
-                <span>{quantity}</span>
-                <button onClick={increase}>+</button>
-            </div>
         )
     }
+    else{
+        return(
+            <div>
+                <button onClick={decrease}>-</button>
+                <span style={{ color: 'black', padding: 0 }}>Quantity - {quantity}</span>
+                <button onClick={increase}>+</button>
+            </div>       
+        )
+    }    
 }
 export default ReduxAddToCart;
